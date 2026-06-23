9e501e2ce293359db608d3fe6d54aa9e86fc42e2
vicke18 <viktor.larsgarde.java25@edu.edugrade.se>
2026-06-23 20:05:22 +0200

    rm

--- src/main/java/se/viktor/fancontrollerlinux/Launcher.java
+++
@@ -1,9 +0,0 @@
-package se.viktor.fancontrollerlinux;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
